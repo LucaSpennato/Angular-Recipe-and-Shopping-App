5837bc6e9de5efa4d9ae1ac51d2b67cf87548b28
Luca Spennato <luca.spennato@gmail.com>
2023-02-26 19:23:54 +0100

    search feature

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,11 +8,21 @@ import { Component } from '@angular/core';
 })
 export class RecipeListComponent {
 
+  recipiesNeedle: string = ''
+
   recipies: Recipe[] = [
-    new Recipe('Test recipe', 'test desc', 
+    new Recipe('pizzazza', 'test desc', 
       'https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2022-05/Tomato-Pie_0256.jpg?itok=c63mh-z9'),
-      new Recipe('Test recipe', 'test desc', 
+      new Recipe('focaccia', 'test desc', 
       'https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2022-05/Tomato-Pie_0256.jpg?itok=c63mh-z9')
   ] 
+
+  getFilteredRecipies(): Recipe[]{
+    if(this.recipiesNeedle === ''){
+      return this.recipies
+    }
+    return this.recipies.filter( recipe => recipe.name.toLowerCase().includes(this.recipiesNeedle.toLowerCase())
+    )
+  }
 
 }
