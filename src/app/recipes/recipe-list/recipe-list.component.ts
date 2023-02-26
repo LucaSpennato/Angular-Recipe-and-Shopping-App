07e9c415b5fcb628ffaa3ef51960ef1fa18f7889
Luca Spennato <luca.spennato@gmail.com>
2023-02-26 12:25:47 +0100

    recipe style adj, more recipe in array

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,8 @@ export class RecipeListComponent {
 
   recipies: Recipe[] = [
     new Recipe('Test recipe', 'test desc', 
+      'https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2022-05/Tomato-Pie_0256.jpg?itok=c63mh-z9'),
+      new Recipe('Test recipe', 'test desc', 
       'https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2022-05/Tomato-Pie_0256.jpg?itok=c63mh-z9')
   ] 
 
