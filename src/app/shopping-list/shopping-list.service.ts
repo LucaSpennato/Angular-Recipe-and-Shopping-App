e55654380ccda656fa68e437ada16492305056b0
Luca Spennato <luca.spennato@gmail.com>
2023-03-01 12:44:24 +0100

    shopping list service methods and props

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,45 @@
-
-
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from './../shared/ingredient.model';
 
 export class ShoppingListService{
 
-    
+    IngredientsChanged = new EventEmitter<Ingredient[]>()
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('PEPPERONI', 2),
+        new Ingredient('Salame', 5),
+        new Ingredient('Tomato', 5)
+    ]
+
+    getIngredients(){
+        return this.ingredients.slice()
+    }
+
+    addIngredient(ingredient: Ingredient){
+        // this.ingredients.push(ingredient)
+
+        let isFound = undefined
+
+        this.ingredients.forEach((ingr,index)=>{
+            if(ingr.name.toLowerCase() === ingredient.name.toLowerCase()){
+                isFound = index
+            }
+        })
+        if(isFound !== undefined){
+            this.ingredients[isFound].amount = +this.ingredients[isFound].amount + +ingredient.amount
+        }else{
+            this.ingredients.push(ingredient)
+        }
+
+        this.IngredientsChanged.emit(this.ingredients.slice())
+    }
+
+    addIngredients(ingredients: Ingredient[]){
+        // for (let ingr of ingredients){
+        //     this.addIngredient(ingr)
+        // }
+        this.ingredients.push(...ingredients)
+        this.IngredientsChanged.emit(this.ingredients.slice())
+    }
 
 }
