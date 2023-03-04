273126e86c4211cfcb6989a78ac2f718d2393520
Luca Spennato <luca.spennato@gmail.com>
2023-03-04 19:50:38 +0100

    recipe edit setup

--- src/app/recipes/reciped-edit/reciped-edit.component.ts
+++ src/app/recipes/reciped-edit/reciped-edit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reciped-edit',
   templateUrl: './reciped-edit.component.html',
   styleUrls: ['./reciped-edit.component.scss']
 })
-export class RecipedEditComponent {
+export class RecipedEditComponent implements OnInit {
 
+  id: number
+  editMode = false
+
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params)=>{
+        this.id = +params['id']
+        // dato che esiste anche la rotta new, controllare se esiste id, ci permette di capire
+        // se siamo in edit o in new senza far crashare l'app
+        this.editMode = params['id'] != null
+      }
+    )
+  }
 }
