4d915cf1f4e2701b7aa3387811f4fdfd1467235a
Luca Spennato <luca.spennato@gmail.com>
2023-03-07 15:04:49 +0100

    get index on click

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -11,24 +11,26 @@ import { Subscription } from 'rxjs';
 export class ShoppingListComponent implements OnInit, OnDestroy {
 
   ingredients: Ingredient[] = []
-  private igChangeSub: Subscription
+  private subscription: Subscription
 
   constructor(private shoppingListService: ShoppingListService ){}
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients()
-    this.igChangeSub = this.shoppingListService.IngredientsChanged.subscribe((ingredients: Ingredient[])=>{
+    this.subscription = this.shoppingListService.IngredientsChanged.subscribe((ingredients: Ingredient[])=>{
       this.ingredients = ingredients
     })
   }
 
   getEmittedIngredient(ingredient: Ingredient){
-
     this.shoppingListService.addIngredient(ingredient)
+  }
 
+  onEditItem(index: number){
+    this.shoppingListService.startedEditing.next(index)
   }
 
   ngOnDestroy(): void {
-    this.igChangeSub.unsubscribe()
+    this.subscription.unsubscribe()
   }
 }
