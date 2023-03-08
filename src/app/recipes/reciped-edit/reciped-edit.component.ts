7f5ea768a5ef3e9e1a754f1ef0bee58fa5540e76
Luca Spennato <luca.spennato@gmail.com>
2023-03-08 09:42:52 +0100

    reactive form on edit and create new recipe

--- src/app/recipes/reciped-edit/reciped-edit.component.ts
+++ src/app/recipes/reciped-edit/reciped-edit.component.ts
@@ -1,5 +1,8 @@
+import { Recipe } from './../recipe.model';
+import { RecipeService } from './../recipe.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reciped-edit',
@@ -11,7 +14,9 @@ export class RecipedEditComponent implements OnInit {
   id: number
   editMode = false
 
-  constructor(private route: ActivatedRoute){}
+  recipeForm: FormGroup
+
+  constructor(private route: ActivatedRoute, private recService: RecipeService){}
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -20,7 +25,71 @@ export class RecipedEditComponent implements OnInit {
         // dato che esiste anche la rotta new, controllare se esiste id, ci permette di capire
         // se siamo in edit o in new senza far crashare l'app
         this.editMode = params['id'] != null
+        this.initForm()
       }
     )
+  }
+
+  private initForm(){
+    let recipeName = ''
+    let recipeImagePath = ''
+    let recipeDescription = ''
+    let recipeingredients = new FormArray([])
+
+    if(this.editMode){
+      const { name, imagePath, description, ingredients } = this.recService.getRecipe(this.id) 
+      recipeName = name
+      recipeImagePath = imagePath
+      recipeDescription = description
+      if(ingredients){
+        for(let ingr of ingredients){
+          recipeingredients.push(
+            new FormGroup({
+              'name': new FormControl(ingr.name, Validators.required),
+              'amount': new FormControl(ingr.amount, [Validators.required, Validators.min(1)]),
+            })
+          )
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+        'name': new FormControl(recipeName, Validators.required),
+        'imagePath': new FormControl(recipeImagePath, Validators.required),
+        'description': new FormControl(recipeDescription, Validators.required),
+        'ingredients': recipeingredients
+    })
+  }
+
+  get ingredientsControls() { 
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  onAddingredient(){
+    // accedo agli ingrediengts, creo un nuovo gruppo che conterrà i form control del nuovo gruppo
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(1, [Validators.required, Validators.min(1)])
+      })
+    )
+  }
+
+  onSubmit(){
+
+    // recipe form essendo totalmente uguale al model, basta passare il recipeform.value senza creare una variabile
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value['name'],
+    //   this.recipeForm.value['description'],
+    //   this.recipeForm.value['imagePath'],
+    //   this.recipeForm.value['ingredients']
+    // )
+
+    if(this.editMode){
+      this.recService.updateRecipe(this.id, this.recipeForm.value)
+    }else{
+      this.recService.addRecipe(this.recipeForm.value)
+    }
+
   }
 }
